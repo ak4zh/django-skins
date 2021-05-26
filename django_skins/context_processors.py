08218eb21f54fa4f0ada809675b266b5c108fd9f
Akash Agarwal <agwl.akash@gmail.com>
2021-05-26 10:41:09 +0530

    context variables format fixed

--- django_skins/context_processors.py
+++ django_skins/context_processors.py
@@ -6,17 +6,17 @@ def template(request):
         Get all settings related to the Django Skins module and return them as context variables
     """
     context = {
-        skin_name = getattr(settings, 'DJANGO_SKIN_NAME', default='volt'),
+        "skin_name": getattr(settings, 'DJANGO_SKIN_NAME', default='volt'),
 
         # view names
-        login_view = getattr(settings, 'DJANGO_SKINS_LOGIN_VIEW_NAME', 'login')
-        logout_view = getattr(settings, 'DJANGO_SKINS_LOGOUT_VIEW_NAME', 'logout')
-        signup_view = getattr(settings, 'DJANGO_SKINS_SIGNUP_VIEW_NAME', 'signup')
+        "login_view": getattr(settings, 'DJANGO_SKINS_LOGIN_VIEW_NAME', 'account_login'),
+        "logout_view": getattr(settings, 'DJANGO_SKINS_LOGOUT_VIEW_NAME', 'account_logout'),
+        "signup_view": getattr(settings, 'DJANGO_SKINS_SIGNUP_VIEW_NAME', 'account_signup'),
 
         # meta data
-        site_meta = getattr(settings, 'DJANGO_SKINS_SITE_META')
-        facebook_meta = getattr(settings, 'DJANGO_SKINS_FACEBOOK_META')
-        twitter_meta = getattr(settings, 'DJANGO_SKINS_TWITTER_META')
+        "site_meta": getattr(settings, 'DJANGO_SKINS_SITE_META'),
+        "facebook_meta": getattr(settings, 'DJANGO_SKINS_FACEBOOK_META'),
+        "twitter_meta": getattr(settings, 'DJANGO_SKINS_TWITTER_META'),
 
     }
     return context
